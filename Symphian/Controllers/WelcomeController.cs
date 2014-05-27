3fd0da08707d33603903c0c3a0b6e98269ea01fe
Matthew Duguay <matthew.duguay@gmail.com>
2014-05-27 13:14:29 -0400

    Move source to GitHub

---
+++ Symphian/Controllers/WelcomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Symphian.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        //
+        // GET: /Welcome/
+
+        public ActionResult Splash()
+        {
+            ViewBag.Message = "Welcome to Symphian. There's more here than meets the eye... really!";
+
+            return View();
+        }
+
+    }
+}
